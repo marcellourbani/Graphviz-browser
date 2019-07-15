1188c711932600399dab23570f04255d2b8f6b3c
Marcello Urbani <marcello.urbani@gmail.com>
2019-07-15 09:06:21 +0100

    removed vulnerabilities

---
+++ precache-manifest.bb8f99c888728e0d6251e391738cedfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a80012c33ec1d5eb81d2bf03508c4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6de0560c0479aa70a9a",
+    "url": "./static/css/main.c1abdbc5.chunk.css"
+  },
+  {
+    "revision": "3285472f8616f01ff888",
+    "url": "./static/js/2.f5576588.chunk.js"
+  },
+  {
+    "revision": "b6de0560c0479aa70a9a",
+    "url": "./static/js/main.90cbc0b2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
