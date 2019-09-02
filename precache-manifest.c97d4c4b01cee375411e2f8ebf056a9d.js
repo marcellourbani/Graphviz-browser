5aad32b3003443507fe5d73b58d89ad27223806f
Marcello Urbani <marcello.urbani@gmail.com>
2019-09-02 14:41:43 +0100

    removed vulnerability

---
+++ precache-manifest.c97d4c4b01cee375411e2f8ebf056a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c680e3f0a571912ea4b6062d2745f40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3db0b80a40cde23923e1",
+    "url": "./static/css/main.c1abdbc5.chunk.css"
+  },
+  {
+    "revision": "97b4b0c31803aa095ff9",
+    "url": "./static/js/2.bb8b1741.chunk.js"
+  },
+  {
+    "revision": "3db0b80a40cde23923e1",
+    "url": "./static/js/main.26a6362e.chunk.js"
+  },
+  {
+    "revision": "5d5f3deef8ea32399c0d",
+    "url": "./static/js/runtime~main.29fc797a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7a0b49769020018cc8f2d38f30fe79a7.js"
+  "./precache-manifest.c97d4c4b01cee375411e2f8ebf056a9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
